b74ce5c58d2c51a80de085382b2c931d9369e2e4
zhiqianggong <1154673531@qq.com>
2020-03-27 15:08:47 +0800

    function

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 ```
 具有功能上一月，下一月，当前年月，点击切换。
 ```
+## myselfFunction
+```
+name: pageTimeout
+function: 页面十五分钟不动退出到index页面
+```
 
 ## Project setup
 ```
---
+++ src/components/myselfFunction/function.vue
@@ -0,0 +1,30 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+         currentTime: new Date().getTime()
+    };
+  },
+  methods: {
+    pageTimeout() {
+      let currentTime = this.currentTime;
+      let lastTime = new Date().getTime();
+      let timeOut = 15 * 60 * 1000;
+      if (lastTime - currentTime > timeOut) {
+        localStorage.clear();
+        this.currentTime = new Date().getTime();
+        this.$router.push("/index");
+      } else {
+        this.currentTime = new Date().getTime();
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
